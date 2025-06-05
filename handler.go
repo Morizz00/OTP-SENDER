c816782af5a06bbc77aaf9b735f2ac6a85938799
Morizz00 <153217551+Morizz00@users.noreply.github.com>
2025-06-05 18:13:30 +0530

    Update handler.go

--- handler.go
+++ handler.go
@@ -52,3 +52,25 @@ func (app *Config) verifySMS() gin.HandlerFunc {
 		app.writeJSON(c, http.StatusAccepted, "OTP verified succesfully")
 	}
 }
+func (app *Config) SendMSG() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		appTimeout := 10 * time.Second
+		_, cancel := context.WithTimeout(context.Background(), appTimeout)
+		var payload data.SendMSGData
+		defer cancel()
+
+		app.validateBody(c, &payload)
+
+		newData := data.SendMSGData{
+			PhoneNumber: payload.PhoneNumber,
+			Message:     payload.Message,
+		}
+
+		err := app.twilioSendMSGWithService(newData.PhoneNumber, newData.Message)
+		if err != nil {
+			app.errorJSON(c, err)
+			return
+		}
+		app.writeJSON(c, http.StatusAccepted, "Message sent successfully")
+	}
+}
